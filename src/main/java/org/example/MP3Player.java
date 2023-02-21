da30847e7a3e73b133ebe8085fc61607514e5e24
jnavarro <jnavarro@fondeguilla.com>
2023-02-21 17:12:30 +0100

    djdfjdj

--- src/main/java/org/example/MP3Player.java
+++ src/main/java/org/example/MP3Player.java
@@ -7,7 +7,11 @@ import javazoom.jl.player.Player;
 public class MP3Player {
     public static void main(String[] args) {
         try {
+<<<<<<< HEAD
             InputStream fis = new FileInputStream("Bandolera.mp3");
+=======
+            InputStream fis = new FileInputStream("ruta/al/archivo.mp3");
+>>>>>>> Initial commit
             Player player = new Player(fis);
             player.play();
         } catch (Exception e) {
