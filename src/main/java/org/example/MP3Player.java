83364fc8db23a9097e5dbde7e6ef83de3db0d712
jnavarro <jnavarro@fondeguilla.com>
2023-02-21 17:12:30 +0100

    djdfjdj

--- src/main/java/org/example/MP3Player.java
+++ src/main/java/org/example/MP3Player.java
@@ -5,13 +5,19 @@ import java.io.InputStream;
 import javazoom.jl.player.Player;
 
 public class MP3Player {
-    public static void main(String[] args) {
-        try {
-            InputStream fis = new FileInputStream("Bandolera.mp3");
-            Player player = new Player(fis);
-            player.play();
-        } catch (Exception e) {
-            System.out.println("Error al reproducir el archivo: " + e.getMessage());
+        public static void play() {
+            try {
+                InputStream fis = new FileInputStream("Bandolera.mp3");
+                Player player = new Player(fis);
+                System.out.println("ESTA SONANDO EL FCKING DIOS DEL TRAP, LA2BLEA. ANUEL.");
+                player.play();
+
+            } catch (Exception e) {
+                System.out.println("Error al reproducir el archivo: " + e.getMessage());
+            }
+
         }
-    }
+
+
+
 }
---
+++ src/main/java/org/example/MainReproducir.java
@@ -0,0 +1,7 @@
+package org.example;
+public class MainReproducir {
+    public static void main(String[] args) {
+        MP3Player mp3Player = new MP3Player();
+        mp3Player.play();
+    }
+}
